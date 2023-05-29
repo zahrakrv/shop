ca81a938f1d5488fce6a571df6a826989a4a4aa7
zahraka <zahrakarvan@gmail.com>
2023-05-29 21:17:27 +0430

    add slider

---
+++ src/pages/slider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SlideShow from '../components/SlideShow';
+
+const Slider = () => {
+  return (
+    <div>
+      <SlideShow />
+    </div>
+  );
+};
+
+export default Slider;
