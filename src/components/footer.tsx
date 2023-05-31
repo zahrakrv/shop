d792819ee5d116d6f7eb71ff05071efcc480daee
zahraka <zahrakarvan@gmail.com>
2023-05-31 13:56:29 +0430

    add layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <p></p>
+    </>
+  );
 };
 
 export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // import Image from 'next/image';
 // import { Inter } from 'next/font/google';
+import Layout from '@/layout/layout';
 import Header from '../components/header';
 import NavBar from './../components/NavBar';
 
@@ -9,8 +10,9 @@ export default function Home() {
   return (
     <>
       <main>
-        <Header />
-        <NavBar />
+        <Layout>
+          <NavBar />
+        </Layout>
       </main>
     </>
   );
